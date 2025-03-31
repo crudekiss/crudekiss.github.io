dd6bca88fb9490ea62c6fcb190df6706669483d9
crudekiss <176537984+crudekiss@users.noreply.github.com>
2025-03-31 19:48:37 +0200

    Update auth.js

--- rotot/auth.js
+++ rotot/auth.js
@@ -18,7 +18,7 @@ fetch('https://crudekiss.github.io/rotot/auth.json')
       window.location.href = 'about:blank';
     } else if (!isAuthorized) {
       // Unauthorized action: Redirect to specified URL
-      window.location.href = 'https://crudekiss.itch.io/rise-of-the-orc-tyrant';
+      window.location.href = 'about:blank';
     }
     // If authorized, do nothing (implicitly allow access)
   })
