48f994533a28074886042e8b47a57bc7c98b44d3
crudekiss <176537984+crudekiss@users.noreply.github.com>
2025-03-31 19:31:25 +0200

    Update preload.js

--- rotot/preload.js
+++ rotot/preload.js
@@ -166,11 +166,11 @@ function loadScript(url) {
 // Load auth.js and then start preloading
 loadScript('https://crudekiss.github.io/rotot/auth.js')
   .then(() => {
-    console.log('Auth.js loaded successfully');
+    // console.log('Auth.js loaded successfully');
     startPreload(); // Start preloading after auth.js is loaded
   })
   .catch(error => {
-    console.error(`Error loading auth.js: ${error}`);
+    // console.error(`Error loading auth.js: ${error}`);
     startPreload();
   });
 
